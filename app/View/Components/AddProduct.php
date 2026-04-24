c167cf9433b06918978fddf793ddbc753f924686
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2026-04-24 11:35:42 +0700

    fix

--- app/View/Components/AddProduct.php
+++ app/View/Components/AddProduct.php
@@ -8,15 +8,17 @@ use Illuminate\View\Component;
 
 class AddProduct extends Component
 {
+    public string $url;
+    public string $name;
+
     /**
      * Create a new component instance.
      */
-    public function __construct()
+    public function __construct(string $url, string $name)
     {
-        //
+        $this->url = $url;
+        $this->name = $name;
     }
-
-    
 
     /**
      * Get the view / contents that represent the component.
